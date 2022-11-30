5308a4f95e8bca1ef92b618feead4bff6d7abe81
catherineyeh26 <113751385+catherineyeh26@users.noreply.github.com>
2022-11-29 22:11:35 -0800

    finished final project

---
+++ final/assets/js/script.js
@@ -0,0 +1,8 @@
+
+
+function toggleteas() {
+    container.colordots.classList.toggle("hidden");
+    container.discriptions.classList.toggle("hidden");
+
+}
+document.getElementById("teas").onhover = toggleteas;
