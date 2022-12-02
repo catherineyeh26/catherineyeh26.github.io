030a482b9cfb64b17b28a8ddd1a8741d7dd60064
catherineyeh26 <113751385+catherineyeh26@users.noreply.github.com>
2022-12-01 17:40:02 -0800

    finished final porject

--- final/assets/js/script.js
+++ final/assets/js/script.js
@@ -1,16 +1,32 @@
 
 
-function toggleteas() {
-    container.colordots.classList.toggle("visible");
-    container.discriptions.classList.toggle("visible");
+// function toggleteas() {
+//     container.colordots.classList.toggle("hidden");
+//     container.discriptions.classList.toggle("hidden");
+
+// }
+
+function toggleColor(color) {
+    document.getElementById("choice").classList.toggle("hidden");
+    Array.prototype.forEach.call(document.getElementsByClassName(color), function(el) {
+        el.classList.toggle("hidden");
+    });
 
 }
-function toggleWhite() {
-    document.getElementById("white_intro").classList.toggle("visible");
-}
-document.getElementById("white_color").onclick = toggleWhite;
-document.getElementById("green_color").onhover = toggleteas;
-document.getElementById("black_color").onhover = toggleteas;
+
+
+document.getElementById("white_color").onmouseover = () => { toggleColor("white") };
+document.getElementById("green_color").onmouseover = () => { toggleColor("green") };
+document.getElementById("black_color").onmouseover = () => { toggleColor("black") };
+document.getElementById("chamomile_color").onmouseover = () => { toggleColor("chamomile") };
+document.getElementById("peppermint_color").onmouseover = () => { toggleColor("peppermint") };
+
+document.getElementById("white_color").onmouseout = () => { toggleColor("white") };
+document.getElementById("green_color").onmouseout = () => { toggleColor("green") };
+document.getElementById("black_color").onmouseout = () => { toggleColor("black") };
+document.getElementById("chamomile_color").onmouseout = () => { toggleColor("chamomile") };
+document.getElementById("peppermint_color").onmouseout = () => { toggleColor("peppermint") };
+
 
 // colordots: position absolute
 // hw 9(from week 9)
